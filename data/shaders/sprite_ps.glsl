c8392624da3628460e914d79cf736fe23e4c2b2c
Rajiv Mohan Gupta <galaxyblogger1@gmail.com>
2018-05-30 13:08:39 +0530

    Add sprite shaders

--- data/shaders/sprite_ps.glsl
+++ data/shaders/sprite_ps.glsl
@@ -12,7 +12,7 @@ out vec4 OutColor;
 
 void main()
 {
-	//OutColor = vec4( pow(fs_in.VertColor.rgb, vec3(2.2)), fs_in.VertColor.a);
+	// OutColor = vec4( pow(fs_in.VertColor.rgb, vec3(2.2)), fs_in.VertColor.a);
 
 	OutColor = texture(Sampler0, fs_in.VertUV) * fs_in.VertColor;
 }
