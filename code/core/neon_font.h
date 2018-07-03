e50b3d3b3982ef2a93a75bb6e6717a6dd2cedafc
Rajiv Mohan Gupta <galaxyblogger1@gmail.com>
2018-07-03 15:54:41 +0530

    Remove FreeType

--- code/core/neon_font.h
+++ code/core/neon_font.h
@@ -1,10 +1,5 @@
 #ifndef NEON_FONT_H
 #define NEON_FONT_H
-
-// Freetype
-#include <ft2build.h>
-#include FT_FREETYPE_H
-
 #include "neon_platform.h"
 #include "neon_bitmap.h"
 
@@ -34,28 +29,4 @@ struct font
     u32 GlyphsCount;
     glyph *Glyphs;
 };
-
-#if 0 
-struct glyph
-{
-    s32 Width;
-    s32 Height;
-    s32 HoriBearingX;
-    s32 HoriBearingY;
-    s32 HoriAdvance;
-    s32 Hang;
-    vec4 Coords;
-};
-
-struct font
-{
-    u32 Height;
-    glyph *Glyphs;
-    render_resource Texture;
-};
-
-void InitFont(font *Font, char const *FontSrcFile, u32 FontHeight);
-void FreeFont(font *Font);
-vec2 GetTextDim(font *Font, char const *Format, ...);
-#endif
 #endif
